0df739f7c2325f4726982960705a35f2092a2daf
Mümin <mozdamar90@hotmail.com>
2022-08-05 12:36:00 +0300

    Derece hesaplama Ödevi tamamlandı.

--- src/CelsiusToFahrenheit.java
+++ src/CelsiusToFahrenheit.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class CelsiusToFahrenheit {
     public static void main(String[] args) {
-        //TODO: Klavyeden girilen Sıcaklık değerleri, diğer birime dönüştürülecektir.
+        //Klavyeden girilen Sıcaklık değerleri, diğer birime dönüştürülecektir.
         /*
             Örnek Menü Tasarımı:
 
@@ -19,6 +21,25 @@ public class CelsiusToFahrenheit {
              30 C --> 86 F
              86 F --> 30 C
          */
+        Scanner input=new Scanner(System.in);
+        int F,C;
+
+        System.out.println("Sıcaklık Dönüşüm Uygulaması");
+        System.out.print("Lütfen bir Sıcaklık Değeri Giriniz : ");
+        int sıcaklıkDegeri= input.nextInt();
+        System.out.println("-------------");
+        System.out.println("1. Cantigrat Derece Hesaplama");
+        System.out.println("2.Fahrenheit Derece Hesaplama ");
+        System.out.print("Lütfen Bir Seçim Yapınız : ");
+        int secim= input.nextInt();
+        switch (secim){
+            case 1:double c=sıcaklıkDegeri-32/1.8;
+                System.out.println("C ="+c+"---->"+'F');
+                break;
+            case 2:double f=sıcaklıkDegeri*1.8+32;
+                System.out.println("F ="+f+"----->"+'C');
+                break;
+        }
 
 
     }
